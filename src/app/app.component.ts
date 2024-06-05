5456ef194211cd7d98fe8b38afa6215c58c57b05
elisavetTerz <eterzani.atm@gmail.com>
2024-06-05 15:45:56 +0200

    Navbar Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+//import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
+
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent]
+    imports: [RouterLink, RouterOutlet, NavbarComponent]
 })
 export class AppComponent {
   title = 'get-your-ticket-app-angular';
